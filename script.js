644efd77ac13bba68f4cd33ee5d3e328c0ceed62
Amayah910 <43344691+Amayah910@users.noreply.github.com>
2019-04-10 09:48:43 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$(".garage-door-opener").click(function() {
+    $(".garage-door").slideUp();
+    $(".garage-door").slideDown();
+    
+});
+
+$(".pokeball").click(function() {
+    $(".pikachu").fadeIn();
+    
+});
+
+$(".light-switch").click(function() {
+    $(".light-bulb").toggle();
+    
+
+});
